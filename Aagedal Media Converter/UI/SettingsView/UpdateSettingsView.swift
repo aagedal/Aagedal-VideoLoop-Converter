59685a6230085150668e514327ce13eb6ccb57f6
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2025-11-30 18:48:30 +0100

    Updated some translations and fixed info about FFMPEG version in the update settings view.

--- Aagedal Media Converter/UI/SettingsView/UpdateSettingsView.swift
+++ Aagedal Media Converter/UI/SettingsView/UpdateSettingsView.swift
@@ -39,7 +39,7 @@ struct UpdateSettingsView: View {
                             .multilineTextAlignment(.center)
                             .padding(.horizontal)
                         
-                        Text("FFMPEG version: 8.0")
+                        Text("FFMPEG version: 8.0.1")
                             .font(.subheadline)
                             .foregroundColor(.secondary)
                     }
